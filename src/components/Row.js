20cc34fb5e8bd3341d92465f7dfd0d64ebbaf5d5
cocoxiang123 <cocoxiang123@users.noreply.github.com>
2020-09-02 15:08:18 +1000

    edit function delete

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,56 +1,72 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Draggable } from 'react-beautiful-dnd';
+import { Draggable } from "react-beautiful-dnd";
 
 function Row({ note, id, handDelete }) {
-    const { title, content } = note;
-    const colors = ['#5cbbfc', '#6ce6dd', '#dfe245', '#ff9aa2'];
-    const mystyle = {
-        backgroundColor: id < colors.length ? `${colors[id]}` : `${colors[id % colors.length]}`,
+  const { title, content } = note;
+  const colors = ["#5cbbfc", "#6ce6dd", "#dfe245", "#ff9aa2"];
+  const mystyle = {
+    backgroundColor:
+      id < colors.length ? `${colors[id]}` : `${colors[id % colors.length]}`,
+  };
+  const borderstyle = {
+    border: `1px solid ${colors[id]}`,
+  };
+  const myFontColor = {
+    color:
+      id < colors.length ? `${colors[id]}` : `${colors[id % colors.length]}`,
+  };
+  const onDelete = (event) => {
+    let result = window.confirm("Want to delete?");
+    if (result) {
+      let id = parseInt(event.target.name);
+      handDelete(id);
     }
-    const borderstyle = {
-        border: `1px solid ${colors[id]}`,
-    }
-    const myFontColor = {
-        color: id < colors.length ? `${colors[id]}` : `${colors[id % colors.length]}`,
-    }
-    const onDelete = (event) => {
-        let result = window.confirm("Want to delete?");
-        if (result) {
-            let id = parseInt(event.target.name);
-            handDelete(id);
-        }
-    }
+  };
 
-    return (
-        <>
-            <Draggable draggableId={`${id}`} index={id} className="dContainer m-1">
-                {(provided, snapshot) => (
-                    <div
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
-                    >
-                        <div className="eachNote" style={borderstyle}>
-                            <div className="displayRowTitle" style={mystyle}>
-                                <h3>Title</h3>
-                                <button name={id} onClick={onDelete} className="btn btn-light btn-sm"> Delete</button>
-                            </div>
-                            <Link to={{ pathname: `/editNote/${id}` }} style={myFontColor} className="titleBody">
+  return (
+    <>
+      <Draggable draggableId={`${id}`} index={id} className="dContainer m-1">
+        {(provided, snapshot) => (
+          <div
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+          >
+            <div className="eachNote" style={borderstyle}>
+              <div className="displayRowTitle" style={mystyle}>
+                <h3>Title</h3>
+                <button
+                  name={id}
+                  onClick={onDelete}
+                  className="btn btn-light btn-sm"
+                >
+                  {" "}
+                  Delete
+                </button>
+              </div>
+
+              {/*             <Link to={{ pathname: `/editNote/${id}` }} style={myFontColor} className="titleBody">
                                 {title}
-                            </Link>
-                            <div className="displayRowTitle" style={mystyle}>
-                                <h3>Note</h3>
-                            </div>
-                            <Link to={{ pathname: `/editNote/${id}` }} style={myFontColor} className="titleBody">
+                            </Link> */}
+              <p style={myFontColor} className="titleBody">
+                {title}
+              </p>
+              <div className="displayRowTitle" style={mystyle}>
+                <h3>Note</h3>
+              </div>
+              {/*  <Link to={{ pathname: `/editNote/${id}` }} style={myFontColor} className="titleBody">
                                 {content}
-                            </Link>
-                        </div>
-                    </div>
-                )}
-            </Draggable>
-        </>
-    );
+                            </Link> */}
+              <p style={myFontColor} className="titleBody">
+                {content}
+              </p>
+            </div>
+          </div>
+        )}
+      </Draggable>
+    </>
+  );
 }
 
 export default Row;
